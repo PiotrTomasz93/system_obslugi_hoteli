64c95c9065ad56ac567d8d81de79637747dc2753
PiotrTomasz93 <piotr.tomasz93@gmail.com>
2020-08-02 17:55:40 +0200

    Initial commit

---
+++ src/main/java/org/tomaszpiotr/App.java
@@ -0,0 +1,8 @@
+package org.tomaszpiotr;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println("Witamy w aplikacji HotelsView. \nWybierz opcję, wpisz odpowiedni numer i zatwierdź klawiszem Enter.");
+    }
+}
